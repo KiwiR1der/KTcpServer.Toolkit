f22bb6ffd6797026a31b9a3991f7ad3cb2c3f2db
robot <robot@major.com>
2025-12-23 17:07:18 +0800

    修改样式

---
+++ TcpServer.Toolkit.Core/Services/IServerService.cs
@@ -0,0 +1,22 @@
+﻿using KTcpServer.Toolkit.Models;
+
+namespace KTcpServer.Toolkit.Core.Services
+{
+    /// <summary>
+    /// “服务提供者”，定义服务行为
+    /// </summary>
+    public interface IServerService
+    {
+        bool IsRunning { get; }
+        Task StartAsync();
+        void Stop();
+        Task SendToClientAsync(Guid clientId, string message);
+        IEnumerable<ClientConnection> GetConnectClients();
+
+        // 事件
+        event Action<string>? OnMessageLogged;
+        event Action<ClientConnection>? OnClientConnected;
+        event Action<Guid>? OnClientDisconnected;
+        event Action<Guid, string>? OnDataReceived;
+    }
+}
---
+++ TcpServer.Toolkit/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using KTcpServer.Toolkit.Core;
+using KTcpServer.Toolkit.Core.Services;
+using KTcpServer.Toolkit.ViewModels;
+
+namespace KTcpServer.Toolkit
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IHost? _host;
+        public App()
+        {
+            ProcessLocker.GetProcessLock();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                // 注册核心服务
+                //services.AddSingleton<IServerService, TcpServer>();
+                services.AddSingleton<IServerService>(sp => new TcpServer(10011));
+
+                // 注册ViewModels
+                services.AddSingleton<MainViewModel>();
+
+                // 注册Views
+                services.AddSingleton<MainWindow>();
+            }).Build();
+
+            // 启动并显示主窗口
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host?.Dispose(); // 清理资源，包括调用TcpServer的Dispose
+            base.OnExit(e);
+        }
+    }
+}
